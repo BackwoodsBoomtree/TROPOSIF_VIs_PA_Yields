df729fa9a66b20b8d0f2ef2a026824dfe9f2b329
Doughty <russell.doughty@ou.edu>
2022-01-27 23:26:32 -0600

    Correct the VI calculations

--- compute_VIS_TROPOMI.R
+++ compute_VIS_TROPOMI.R
@@ -15,43 +15,60 @@ add_VIs <- function(tropomi_file){
   ref_665 <- ncvar_get(myfile, varid = "PRODUCT/SUPPORT_DATA/DETAILED_RESULTS/TOA_RFL")[1,]
   ref_781 <- ncvar_get(myfile, varid = "PRODUCT/SUPPORT_DATA/DETAILED_RESULTS/TOA_RFL")[7,]
   
-  # Calculate VIs
-  ndvi     <- (ref_781 - ref_665) / (ref_781 + ref_665)
-  nirv     <- ndvi * ref_781
-  nirv_rad <- ndvi * rad
+  # Calculate VIs (can return NaN when dividing by 0, so we need to do this recursively)
+  ndvi     <- c()
+  nirv     <- c()
+  nirv_rad <- c()
+  
+  for (i in 1:length(ref_665)) {
+    if (ref_665[i] != 0 && ref_781[i] != 0){
+      ndvi[i]  <- (ref_781[i] - ref_665[i]) / (ref_781[i] + ref_665[i])
+      nirv[i]  <- ndvi[i] * ref_781[i]
+      nirv_rad[i] <- ndvi[i] * rad[i]
+    } else {
+      ndvi[i]     <- 0
+      nirv[i]     <- 0
+      nirv_rad[i] <- 0
+    }
+  }
   
   # Close then open for writing
   nc_close(myfile)
   myfile <- nc_open(tropomi_file, write = TRUE)
   
-  elem_dim <- myfile$dim[['n_elem']]
-  
-  # Define variables that will be added to nc file
-  ndvi_var     <- ncvar_def("PRODUCT/NDVI", units ="-", dim = elem_dim, longname = "Normalized Difference Vegetation Index",
-                    pre = "float", compression = 4, missval = -9999)
-  nirv_var     <- ncvar_def("PRODUCT/NIRv", units ="-", dim = elem_dim, longname = "NIR Reflectance of Vegetation",
-                    pre = "float", compression = 4, missval = -9999)
-  nirv_rad_var <- ncvar_def("PRODUCT/NIRv_RAD", units ="mW/m2/sr/nm", dim = elem_dim, longname = "NIRv Radiance",
-                    pre = "float", compression = 4, missval = -9999)
-  
-  # Add the variables to the file
-  ncvar_add(myfile, ndvi_var)
-  ncvar_add(myfile, nirv_var)
-  ncvar_add(myfile, nirv_rad_var)
-  
-  # Close then open for writing
-  nc_close(myfile)
-  myfile <- nc_open(tropomi_file, write = TRUE)
+  # elem_dim <- myfile$dim[['n_elem']]
+  # 
+  # # Define variables that will be added to nc file
+  # ndvi_var     <- ncvar_def("PRODUCT/NDVI", units ="-", dim = elem_dim, longname = "Normalized Difference Vegetation Index",
+  #                   pre = "float", compression = 4, missval = -9999)
+  # nirv_var     <- ncvar_def("PRODUCT/NIRv", units ="-", dim = elem_dim, longname = "NIR Reflectance of Vegetation",
+  #                   pre = "float", compression = 4, missval = -9999)
+  # nirv_rad_var <- ncvar_def("PRODUCT/NIRv_RAD", units ="mW/m2/sr/nm", dim = elem_dim, longname = "NIRv Radiance",
+  #                   pre = "float", compression = 4, missval = -9999)
+  # 
+  # # Add the variables to the file
+  # ncvar_add(myfile, ndvi_var)
+  # ncvar_add(myfile, nirv_var)
+  # ncvar_add(myfile, nirv_rad_var)
+  # 
+  # # Close then open for writing
+  # nc_close(myfile)
+  # myfile <- nc_open(tropomi_file, write = TRUE)
+  # 
+  # # Write data
+  # ncvar_put(myfile, ndvi_var, ndvi)
+  # ncvar_put(myfile, nirv_var, nirv)
+  # ncvar_put(myfile, nirv_rad_var, nirv_rad)
   
   # Write data
-  ncvar_put(myfile, ndvi_var, ndvi)
-  ncvar_put(myfile, nirv_var, nirv)
-  ncvar_put(myfile, nirv_rad_var, nirv_rad)
+  ncvar_put(myfile, "PRODUCT/NDVI", ndvi)
+  ncvar_put(myfile, "PRODUCT/NIRv", nirv)
+  ncvar_put(myfile, "PRODUCT/NIRv_RAD", nirv_rad)
   
   # Close
   nc_close(myfile)
   
-  print(paste0("Done with ", basename(i)))
+  print(paste0("Done with ", basename(tropomi_file)))
   
 }
 
