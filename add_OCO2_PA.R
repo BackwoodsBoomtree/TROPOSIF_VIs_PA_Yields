340aa2ca2f91fa93bcc88f1f6131736c7edd78f7
Doughty <russell.doughty@ou.edu>
2022-09-22 12:36:26 -0500

    Working version

--- add_OCO2_PA.R
+++ add_OCO2_PA.R
@@ -1,7 +1,7 @@
 library(ncdf4)
 library(parallel)
 
-file_dir <- "/mnt/g/OCO2/B11/original"
+file_dir <- "/mnt/g/OCO2/B11/test"
 
 file_list <- list.files(file_dir, recursive = TRUE, full.names = TRUE, pattern = "*.nc")
 
